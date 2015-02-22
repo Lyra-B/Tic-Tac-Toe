a97876239ad0e4c7db693f9d533ab6f95f5a3581
Lyra-B <glykeriapeppa@gmail.com>
2015-02-22 17:52:07 +0000

    The game starts again if we have a winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,9 +9,6 @@ class Board
 		]
 	end
 
-	# def diagonals
-	# 	right_down = []
-	# 	3.times {||}
   # TODO - Add code so that the array at co-ordinate x, y is set to the value
   # of marker, unless it has already been set.
   #
@@ -19,58 +16,30 @@ class Board
 		if @board[y-1][x-1] == "."
 			@board[y-1][x-1] = marker
 			puts @board[y-1][x-1]
-      puts display_board
+      puts to_s
     else
     	puts "Sorry.Play elsewhere!"
     	return false
     end
-    # binding.pry
 	end
 
 	# TODO - Have the board return each of the possible winning combinations.
-	#
-	# def combinations
-	# 	@board[0]
-	# 	@board[1]
- #    @board[2]
- #    columns = @board.transpose
- #    columns[0]
- #    columns[1]
- #    columns[2]
- #    diagonal_one = [@board[0][0], @board[1][1], @board [2][2]]
- #    diagonal_two = [@board[0][2], @board[1][1], @board [2][0]]
 
 	def each_winning_move
 		columns = @board.transpose
 		diagonal_one = [@board[0][0], @board[1][1], @board [2][2]]
     diagonal_two = [@board[0][2], @board[1][1], @board [2][0]]
-		# possible_combinations = [@board[0], @board[1], @board[2], columns[0], columns[1], 
-		# columns[2], diagonal_one, diagonal_two]
-		3.times do |i|
-		yield @board[i]
-		end
 
-		3.times do |i|
-		yield columns[i]
-		end
+		possible_combinations = [@board[0], @board[1], @board[2], columns[0], columns[1],
+		columns[2], diagonal_one, diagonal_two]
 
- 		yield diagonal_one
- 		yield diagonal_two
-
-		# 3.times do |x|
-		# 	3.times do |y|
-		# 		@board[x-1][y-1]
-		# 	end
-		# end
+		possible_combinations.each {|n| yield n}
 	end
 
 	# TODO - Add code to return the board as a String, so that it appears
 	# in a 3 x 3 grid
-	def to_s
-		return @board.to_s
-	end
 
-	def display_board
+	def to_s
 		i=0
 		while i<3
 			puts @board[i].to_s
@@ -86,6 +55,16 @@ class Game
 		@turn = @players.sample
 	end
 
+  def ask_player
+    puts "Give me your coordinates!"
+    coord = gets.strip.chomp.split(",")
+    if coord[0].to_i > 3 || coord[1].to_i > 3
+      puts "You are outside the board!Try again!"
+      ask_player
+    end
+    coord
+  end
+
   def change_player
   	if @turn == Cross
   		@turn = Nought
@@ -94,26 +73,37 @@ class Game
     end
   end
 
+ #  counts = 0
+	# def draw
+	# 	if next_turn
+	# 		counts+=1
+	# 	end
+	# 	if counts == 8
+	# 		puts "No winner! We have a draw"
+	# 		puts "Would you like to play again?"
+	# 		answer = gets.strip.chomp.downcase
+	# 		if answer == 'yes'
+	# 		Game.new.play
+	# 	  end
+	# 	end
+	# end
+
 	# TODO - The main game loop goes in here.
 	#
 	def play
 		# While the game is still going on, do the following:
 			# 1. Show the board to the user
 
-    
+
     marking = false
     while marking == false
-    	user_input = ask_player
-    	marking = @board.mark(user_input[0].to_i,user_input[1].to_i, @turn.marker)
+    	coord = ask_player
+    	marking = @board.mark(coord[0].to_i,coord[1].to_i, @turn.marker)
 		end
-		winner
+			winner
+		#unless draw
 			next_turn
-
-		
-
-
-
-			# 2. Prompt for an co-ordinate on the Board that we want to target
+      # 2. Prompt for an co-ordinate on the Board that we want to target
 			# 3. Mark the board on the given square. If the input is invalid or already
 			# taken, go back to 1.
 			# 4. If we've got a winner, show the board and show a congratulations method.
@@ -122,11 +112,6 @@ class Game
 	end
   # TODO - Return the next player's turn. I.e. not @turn but the other one.
   #
-  def ask_player
-		puts "Give me your coordinates!"
-	  user_input = gets.strip.chomp.split(",")
-	end
-
 	def next_turn
 		change_player
 		play
@@ -137,16 +122,16 @@ class Game
 	def winner
 		# Check each of the winning moves on the board, rows, cols and diagonals
 		# to see if a Player has filled a row of three consequtive squares
-    @board.each_winning_move do |line|
+    @board.each_winning_move do |move|
     	i=0
-    	line.each do |value|
-    		if value == @turn.marker
-    			i +=1
-    		end
+    	move.each do |spot|
+    			i += 1 if spot == @turn.marker
     	end
-    	if i == 3
-    		puts "And we have a winner!"
-    	end
+      if i == 3
+      	puts "You are the winner!Congratulations!"
+        puts "Now let's play again!"
+        Game.new.play
+      end
     end
   end
 end
@@ -159,13 +144,13 @@ end
 
 class Nought < Player
 	def self.marker
-		'o'
+		'O'
 	end
 end
 
 class Cross < Player
 	def self.marker
-		'x'
+		'X'
 	end
 end
 
